763303f6128a442efcc74222d3ef8782e6db33f1
Valisoa Nomena Loic Randrianaivo <varandri@c1r10s4.42antananarivo.mg>
2026-01-20 15:36:38 +0300

    strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 08:20:02 by varandri          #+#    #+#             */
+/*   Updated: 2026/01/20 14:11:49 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	i = 0;
+	while (i < (size - 1))
+	{
+		if (!*(src + i))
+			break ;
+		*(dst + i) = *(src + i);
+		i ++;
+	}
+	*(dst + i) = '\0';
+	return (src_len);
+}
+
+// #include <stdio.h> 
+// #include <string.h> 
+// int main() 
+// { 
+//   char dest[4]="ggg"; 
+//   char test[]="test";
+//   printf("%zu \n" ,ft_strlcpy(dest , test , 4));
+//   printf("%zu \n" , ft_strlen(dest));
+//   printf("%s", dest); 
+// return 0; 
+// }
