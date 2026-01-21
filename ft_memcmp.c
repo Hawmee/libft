371a27cffb00d438c6f1fdf278ff3e20d27fb2e4
Valisoa Nomena Loic Randrianaivo <varandri@c1r5s12.42antananarivo.mg>
2026-01-21 09:44:12 +0300

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 08:50:52 by varandri          #+#    #+#             */
+/*   Updated: 2026/01/21 09:41:49 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	i = 0;
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while ((*(str1 + i) == *(str2 + i)) && i < (n - 1))
+		i ++;
+	return ((char)(*(str1 + i)) - (char)(*(str2 + i)));
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <strings.h>
+
+// int main()
+// {
+
+// 	char a[8]="atmos\0\0";
+// 	char b[8]="atmos\0\0";
+// 	printf("%i \n" , ft_memcmp(a, b , 9));
+// 	printf("%i" , memcmp(a, b , 9));
+// 	return 0;
+// }
