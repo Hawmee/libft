e1b8d44884301e0bce0d45e8874aa447d7648c81
Valisoa Nomena Loic Randrianaivo <varandri@c1r5s12.42antananarivo.mg>
2026-01-21 13:51:48 +0300

    libft25

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 08:20:25 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/21 12:36:29 by varandri         ###   ########.fr       */
+/*   Updated: 2026/01/21 13:51:39 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,6 @@ void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_atoi(const char *nptr);
+char	*ft_strdup(const char *s);
+
 #endif
