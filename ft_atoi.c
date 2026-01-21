59c08b19b5fd070130223f6e00d598ed448f7cb0
Valisoa Nomena Loic Randrianaivo <varandri@c1r5s12.42antananarivo.mg>
2026-01-21 12:48:35 +0300

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 12:34:16 by varandri          #+#    #+#             */
+/*   Updated: 2026/01/21 12:47:43 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	i;
+	int	sign;
+	int	nbr;
+
+	i = 0;
+	sign = 1;
+	nbr = 0 ;
+	while (*(nptr + i) == 32 || (*(nptr + i) >= 9 && *(nptr + i) <= 13))
+	{
+		i ++;
+	}
+	if (*(nptr + i) == '-')
+	{
+		sign = -sign;
+		i ++ ;
+	}
+	while (ft_isdigit(*(nptr + i)))
+	{
+		nbr = nbr * 10 + (*(nptr + i) - '0');
+		i ++;
+	}
+	return (sign * nbr);
+}
+
+// #include <stdio.h>
+
+// int main (void)
+// {
+// 	printf("%i" , ft_atoi("       -12a332"));	
+// 	return (0);
+// }
