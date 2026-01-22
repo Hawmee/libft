b8b65ee177d017f2d38646780ec48cc7a843682f
Valisoa Nomena Loic Randrianaivo <varandri@c1r5s14.42antananarivo.mg>
2026-01-22 15:04:30 +0300

    strstrim(libft)

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,36 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 09:59:48 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/22 12:20:46 by varandri         ###   ########.fr       */
+/*   Updated: 2026/01/22 15:04:09 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
+static void	remove_front(char *s1)
+{
+	ft_memmove(s1, (s1 + 1), ft_strlen(s1 + 1));
+	*(s1 + (int)ft_strlen(s1) - 1) = '\0';
+}
+
+static void	remove_back(char *s2)
+{
+	*(s2 + (int)ft_strlen(s2) - 1) = '\0';
+}
+
+static int	is_set(int c, char *set)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(set + i))
+	{
+		if ((int)*(set + i) == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -21,16 +45,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 	i = 0;
 	if (!s1)
 		return (NULL);
-	while (s1 && set && *(set + i))
+	while (set && is_set(*(s1), (char *)set))
 	{
-		if (*(set + i) == *(s1))
-		{
-			ft_memmove((char *)s1, (s1 + 1), ft_strlen(s1 + 1));
-			*((char *)(s1 + (int)ft_strlen(s1) - 1)) = '\0';
-		}
-		if (*(set + i) == *(s1 + (int)ft_strlen(s1) - 1))
-			*((char *)(s1 + (int)ft_strlen(s1) - 1)) = '\0';
-		i ++;
+		remove_front((char *)s1);
+	}
+	while (set && is_set(*(s1 + (int)ft_strlen(s1) - 1), (char *)set))
+	{
+		remove_back((char *)s1);
 	}
 	res = (char *)malloc ((int)ft_strlen(s1) * (int) sizeof(char));
 	if (!res)
@@ -44,9 +65,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 // int	main(void)
 // {
-// 	char c[10] = "hellllo";
+// 	char c[10] = "hello ";
 // 	// char *c = NULL;
 // 	printf("'%s' \n" , c);
-// 	printf("'%s'" , ft_strtrim(c , "hfo"));
+// 	printf("'%s'" , ft_strtrim(c , "hl"));
 // 	return (0);
 // }
