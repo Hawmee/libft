d1803317184e66fff668cb380cf86b42199e88ee
Valisoa Nomena Loic Randrianaivo <varandri@c1r9s4.42antananarivo.mg>
2026-01-23 09:44:11 +0300

    bzero::optimized

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 14:10:47 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/22 09:35:33 by varandri         ###   ########.fr       */
+/*   Updated: 2026/01/23 09:43:38 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	ft_memset((unsigned char *)s , 0 , n);
+	ft_memset((unsigned char *)s, 0, n);
 }
-
-// #include <stdio.h>
-// #include <strings.h>
-// #include <bsd/string.h>
-
-// int	main(void)
-// {
-// 	char c[12]= "aaaaaaaa" ;
-	
-//     bzero(&c[2] , 1);
-//     int i;
-// 	for(i = 0 ; i < (int)ft_strlen(c)*(int)sizeof(int) ; i ++)
-// 	{
-// 		printf("%i" , c[i]);
-// 	}
-// 	return (0);
-// }
