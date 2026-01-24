e90d812ab9a138e6b13b6b7066452f4a96bb3b97
Valisoa Nomena Loic Randrianaivo <varandri@c1r10s3.42antananarivo.mg>
2026-01-24 08:56:45 +0300

    itoa fixed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 15:45:33 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/24 01:15:00 by varandri         ###   ########.fr       */
+/*   Updated: 2026/01/24 08:56:22 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ static int	ft_int_len(int n)
 		i = i + 1;
 		n = -n;
 	}
-	if (n == -2147483648)
-		return (11);
-	while (n % 10)
+	while (n)
 	{
 		i ++;
 		n = n / 10;
@@ -32,16 +30,31 @@ static int	ft_int_len(int n)
 	return (i);
 }
 
+static void	ft_convert(int n, char *str, int int_len)
+{
+	int	i;
+
+	i = 0;
+	while (n)
+	{
+		*(str + (int_len - 1) - i) = (n % 10) + '0';
+		n = n / 10;
+		i ++;
+	}
+	*(str + int_len) = '\0';
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;	
-	int		i;
 	int		int_len;
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
+	if (n == 0)
+		return (ft_strdup("0"));
 	int_len = ft_int_len(n);
-	str = (char *)malloc ((sizeof(char) * int_len) + 1);
+	str = (char *)malloc (sizeof(char) * (int_len + 1));
 	if (!str)
 		return (NULL);
 	if (n < 0)
@@ -49,13 +62,6 @@ char	*ft_itoa(int n)
 		n = -n;
 		*(str + 0) = '-';
 	}
-	i = 0;
-	while (n % 10)
-	{
-		*(str + (int_len - 1) - i) = (n % 10) + '0';
-		n = n / 10;
-		i ++;
-	}
-	*(str + int_len) = '\0';
+	ft_convert(n, str, int_len);
 	return (str);
 }
