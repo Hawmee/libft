ee18379ac84ecf784f681485309ed953b82d042a
Valisoa Nomena Loic Randrianaivo <varandri@c1r10s3.42antananarivo.mg>
2026-01-24 12:47:59 +0300

    substr fixed

--- ft_substr.c
+++ ft_substr.c
@@ -6,28 +6,29 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 15:41:48 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/21 17:17:57 by varandri         ###   ########.fr       */
+/*   Updated: 2026/01/24 12:16:17 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
+#include <stdio.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sb;
 	size_t	i;
 
-	sb = (char *)malloc (sizeof(char) * len);
+	(void) start;
+	sb = (char *)malloc (sizeof(char) * (len + 1));
 	if (!sb || !s)
 		return (NULL);
 	i = 0;
-	while (len -- && *(s + (int)start + (int)i))
+	while (i < len && s[(int)start + (int)i])
 	{
-		*(sb + i) = *((char *)(s + (int)start + (int)i));
+		*(sb + i) = (char)(*((char *)(s + (int)start + (int)i)));
 		i ++;
 	}
-	*(sb + i) = '\0';
+	sb[i] = '\0';
 	return (sb);
 }
 
@@ -35,10 +36,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 // int	main(void)
 // {
-// 	char t[5]="hell";
+// 	char t[30]="hello world mf";
 
 // 	char *sb;
-// 	sb = ft_substr(t , 1 , 10);
+// 	sb = ft_substr(t , 6 , 5);
 // 	printf("'%s' \n" , sb);
 // 	return (0);
 // }
