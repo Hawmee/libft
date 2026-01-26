1de8e08a9b035d039acd9fe7ce9bd7f3900501fe
Valisoa Nomena Loic Randrianaivo <varandri@c1r9s5.42antananarivo.mg>
2026-01-26 16:44:05 +0300

    lst_clear

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/26 16:36:23 by varandri          #+#    #+#             */
+/*   Updated: 2026/01/26 16:43:04 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*temp;
+	t_list	*actual;
+
+	temp = *lst;
+	actual = NULL;
+	if (!*lst || !del)
+		return ;
+	while (temp)
+	{
+		actual = temp;
+		del(temp->content);
+		temp = temp->next;
+		free(actual);
+	}
+}
