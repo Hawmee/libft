371b915a0546dbe0c64a2247031650e66fc32199
Valisoa Nomena Loic Randrianaivo <varandri@student.42antananarivo.mg>
2026-02-06 08:18:23 +0300

    maybe the last (3, strjoin optimal)

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,45 +6,41 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 08:46:52 by varandri          #+#    #+#             */
-/*   Updated: 2026/02/06 08:02:52 by varandri         ###   ########.fr       */
+/*   Updated: 2026/02/06 08:12:09 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_alloc(char **res, char const *s1, char const *s2)
-{
-	size_t	res_len;
+// static void	ft_alloc(char **res, char const *s1, char const *s2)
+// {
+// 	size_t	res_len;
 
-	res_len = ft_strlen(s1) + ft_strlen(s2);
-	*res = (char *)ft_calloc ((res_len + 1), sizeof(char));
-}
+// 	res_len = ft_strlen(s1) + ft_strlen(s2);
+// 	*res = (char *)ft_calloc ((res_len + 1), sizeof(char));
+// }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*res;
+	size_t	res_len;
 
 	if (!s1 && !s2)
 		return (NULL);
 	else if (!s1 && s2)
-	{
 		res = ft_strdup(s2);
-		if (!res)
-			return (NULL);
-	}
 	else if (s1 && !s2)
-	{
 		res = ft_strdup(s1);
-		if (!res)
-			return (NULL);
-	}
 	else
 	{
-		ft_alloc(&res, s1, s2);
+		res_len = ft_strlen(s1) + ft_strlen(s2);
+		res = (char *)ft_calloc ((res_len + 1), sizeof(char));
 		if (!res)
 			return (NULL);
 		ft_memmove(res, s1, ft_strlen(s1));
 		ft_memmove((res + ft_strlen(s1)), s2, ft_strlen(s2));
 	}
+	if (!res)
+		return (NULL);
 	return (res);
 }
