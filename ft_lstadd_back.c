4a43cfeb0fad2be5f7530f4d80cce785a8163761
Valisoa Nomena Loic Randrianaivo <varandri@student.42antananarivo.mg>
2026-02-06 07:30:16 +0300

    maybe the last change

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 08:20:30 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/28 06:51:57 by varandri         ###   ########.fr       */
+/*   Updated: 2026/02/06 07:28:12 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
-	if (!new)
+	if (!new || !lst)
 		return ;
 	if (!*lst)
 	{
--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 08:00:43 by varandri          #+#    #+#             */
-/*   Updated: 2026/01/26 15:27:39 by varandri         ###   ########.fr       */
+/*   Updated: 2026/02/06 07:28:26 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	if (!new)
+	if (!new || !lst)
 		return ;
 	if (!*lst)
 	{
