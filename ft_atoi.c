c9faf254dee62f02c73834d95782ff97951042b2
Hawmee <nomenaloicrandriann@gmail.com>
2026-02-08 23:40:52 +0300

    libft: Done & Clean

--- ft_atoi.c
+++ ft_atoi.c
@@ -20,14 +20,14 @@ int	ft_atoi(const char *nptr)
 
 	i = 0;
 	sign = 1;
-	nbr = 0 ;
+	nbr = 0;
 	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
 		i ++;
 	if (nptr[i] == '-' || nptr[i] == '+')
 	{
 		if (nptr[i] == '-')
 			sign = -sign;
-		i ++ ;
+		i ++;
 	}
 	while (ft_isdigit(nptr[i]))
 	{
--- ft_memset.c
+++ ft_memset.c
@@ -19,7 +19,7 @@ void	*ft_memset(void *str, int c, size_t n)
 	ptr = (unsigned char *)str;
 	while (n)
 	{
-		*ptr = (unsigned char)c ;
+		*ptr = (unsigned char)c;
 		ptr ++;
 		n --;
 	}
